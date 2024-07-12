98b17467e80c8d7ce7a42041705858849ba5690b
Garrett Smith <lamelyte@gmail.com>
2024-07-12 14:26:41 -0500

    continue to build signup page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,5 @@
-// import './Login.css';
-import crosswalk from '../images/big-crosswalk.jpg';
+import './SignUp.css';
+import man_smiling from '../images/man-smiling.jpg';
 
 export function SignUp() {
     return (
@@ -10,7 +10,7 @@ export function SignUp() {
                         <div class="card border-light-subtle shadow-sm">
                             <div class="row g-0">
                                 <div class="col-12 col-md-6">
-                                    <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy" src="./assets/img/logo-img-1.webp" alt="Welcome back you've been missed!"/>
+                                    <img class="img-fluid rounded-start w-100 h-100 object-fit-cover" loading="lazy" src={man_smiling} alt="Welcome back you've been missed!"/>
                                 </div>
                                 <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
                                     <div class="col-12 col-lg-11 col-xl-10">
@@ -18,26 +18,14 @@ export function SignUp() {
                                             <div class="row">
                                                 <div class="col-12">
                                                     <div class="mb-5">
-                                                        <div class="text-center mb-4">
+                                                        {/* <div class="text-center mb-4">
                                                             <a href="#!">
                                                                 <img src="./assets/img/bsb-logo.svg" alt="BootstrapBrain Logo" width="175" height="57"/>
                                                             </a>
-                                                        </div>
-                                                        <h2 class="h4 text-center">Registration</h2>
+                                                        </div> */}
+                                                        <h2 class="h2 text-center" id="welcome">Welcome!</h2>
+                                                        <p class="text-center" id="tagline">Create a new MyCitadel account:</p>
                                                     </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-12">
-                                                    <div class="d-flex gap-3 flex-column">
-                                                        <a href="#!" class="btn btn-lg btn-outline-dark">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
-                                                                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
-                                                            </svg>
-                                                            <span class="ms-2 fs-6">Log in with Google</span>
-                                                        </a>
-                                                    </div>
-                                                    <p class="text-center mt-4 mb-5">Or enter your details to register</p>
                                                 </div>
                                             </div>
                                             <form action="#!">
@@ -62,7 +50,7 @@ export function SignUp() {
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="form-floating mb-3">
-                                                            <input type="password" class="form-control" name="password" id="password" value="" placeholder="Password" required/>
+                                                            <input type="password" class="form-control" name="password" id="password" placeholder="Password" required/>
                                                                 <label for="password" class="form-label">Password</label>
                                                         </div>
                                                     </div>
@@ -76,11 +64,25 @@ export function SignUp() {
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="d-grid">
-                                                            <button class="btn btn-dark btn-lg" type="submit">Sign up</button>
+                                                            <button class="btn btn-dark btn-lg mt-3" id="signup" type="submit">Sign up</button>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </form>
+                                            <div class="row">
+                                                <div class="col-12">
+                                                <p class="text-center mt-3 mb-3">Or register with your Google account</p>
+                                                    <div class="d-flex gap-3 flex-column">
+                                                        <a href="#!" class="btn btn-lg btn-outline-dark">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
+                                                                <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
+                                                            </svg>
+                                                            <span class="ms-2 fs-6">Log in with Google</span>
+                                                        </a>
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
                                             <div class="row">
                                                 <div class="col-12">
                                                     <p class="mb-0 mt-5 text-secondary text-center">Already have an account? <a href="#!" class="link-primary text-decoration-none">Sign in</a></p>
